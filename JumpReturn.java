b4820fb42d9cd312239e2c277975d995cdefd142
ADARSH AIYAPPA KM <kalimadaadarsh3@gmail.com>
2026-03-26 14:31:02 +0530

    updated controle structure

---
+++ JumpReturn.java
@@ -0,0 +1,9 @@
+public class JumpReturn {
+    public static void main(String[] args) {
+        System.out.println("before return statment");
+        return;
+        //this is not allowed and gives error
+       // System.out.println("error after return statment");
+    }
+    
+}
