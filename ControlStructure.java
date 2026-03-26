77f10c2a349866cfa1943f3c552cf7ee7646db82
ADARSH AIYAPPA KM <kalimadaadarsh3@gmail.com>
2026-03-26 10:14:44 +0530

    update control structure

---
+++ ControlStructure.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ControlStructure {
+    public static void main(String[] args) {
+        // int age=18;
+        // Scanner sc=new Scanner(System.in);
+        // System.out.println("Enter the age");
+        // int age=sc.nextInt(); 
+        // if (age>=18) 
+        //     System.out.println("eligible");
+        // sc.close();
+        //if else  biggest of two numbers
+            Scanner sc=new Scanner(System.in);
+            int a=sc.nextInt();
+            System.out.println(:"Enter second number");
+            int b=sc.nextInt();
+            if(a>b)
+                System.out.println("First no is largest");
+            else
+                System.out.println("Second number is largest")
+   }
+    
+}
+
