96b94971ec367afac46daf6f00644da1facb24b0
ADARSH AIYAPPA KM <kalimadaadarsh3@gmail.com>
2026-03-26 15:11:40 +0530

    updated controle structure

---
+++ PrimOrNot.java
@@ -0,0 +1,26 @@
+public class PrimOrNot {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter a number");
+        int n=sc.nextInt();
+        boolean flag=true;
+        if(n<=1){
+            flag=false;
+        }
+        else{
+            for(int i=2;i<=Math.sqrt(n);i++){
+                if(n%i==0){
+                    flag=false;
+                    break;
+                }
+            }
+        }
+        if (flag)
+             System.out.println(n+"is a prime number");
+        else 
+             System.out.println(n+"is not a prime number");
+        sc.close();
+  
+    }
+    
+}
