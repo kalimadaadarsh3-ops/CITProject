daef94fb6be5bdfc71851cf4dfded728fcd31d7d
ADARSH AIYAPPA KM <kalimadaadarsh3@gmail.com>
2026-03-26 13:09:52 +0530

    updated controle structure

---
+++ JumpBreak.java
@@ -0,0 +1,11 @@
+public class JumpBreak {
+    public static void main(String[] args) {
+        for (int i=1;i<=10;i++){
+            if(i==5)
+                  // break;
+            continue;
+            System.out.println("val;ue is:"+i);
+        }
+    }
+    
+}
